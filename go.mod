8caff7b126df64840e737b6a2f568926d9d27a2b
AYn0nyme <aynoblox@gmail.com>
2025-05-29 12:52:39 -0400

    added slash commands instead of message commands

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.24.2
 
 require (
 	github.com/joho/godotenv v1.5.1
-	godiscord.foo.ng/lib v0.0.0-20250527094122-0c0fcf28acb0
+	godiscord.foo.ng/lib v0.0.0-20250529140628-0144b7217a58
 )
 
 require github.com/gorilla/websocket v1.5.3 // indirect
--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	"godiscord.foo.ng/lib/pkg/classes"
 	"godiscord.foo.ng/lib/pkg/enums"
+	"godiscord.foo.ng/lib/pkg/slash"
+	"godiscord.foo.ng/lib/pkg/types"
 
 	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
 	Client := classes.NewClient(
+		os.Getenv("DISCORD_TOKEN"),
 		enums.GatewayIntent.Guilds,
 		enums.GatewayIntent.GuildMembers,
 		enums.GatewayIntent.GuildMessages,
@@ -20,26 +22,55 @@ func main() {
 		enums.GatewayIntent.MessageContent,
 	)
 
+	slash.RegisterGuildCommands("1375914465064915144", []classes.SlashCommandData{
+		{
+			Name:        "ping",
+			Description: "Pong! Get the ping of the bot",
+			Type:        enums.InteractionType.ChatInput,
+		},
+		{
+			Name:        "links",
+			Description: "Get the links of the project",
+			Type:        enums.InteractionType.ChatInput,
+		},
+	}, "1373794354677813290")
+
 	Client.On("READY", func(args ...any) {
 		fmt.Println("READY:", args[0].(classes.Client).User.Username)
+		Client.SetPresence(classes.PresenceUpdate{
+			Activities: []classes.Activity{
+				classes.Activity{
+					Name: "godiscord",
+					Type: enums.ActivityType.Streaming,
+					URL:  "https://twitch.tv/godiscord",
+				},
+			},
+			Status: "dnd",
+			AFK:    false,
+		})
 	})
 
-	Client.On("MESSAGE_CREATE", func(args ...any) {
-		message := args[0].(classes.Message)
-		//var message_args []string
-		commandName := strings.Fields(message.Content)[0]
-		if len(strings.Fields(message.Content)) > 1 {
-			//message_args = strings.Fields(message.Content)[1:]
-		}
-		if commandName == "!ping" {
-			message.Reply("Pong!")
-		}
-		if commandName == "!gh" || commandName == "!github" {
-			message.Reply("https://github.com/AYn0nyme/godiscord")
+	Client.On("INTERACTION_CREATE", func(args ...any) {
+		interaction := args[0].(classes.BaseInteraction)
+		if interaction.Data.Name == "ping" {
+			interaction.Reply(classes.MessageData{
+				Embeds: []classes.Embed{
+					classes.NewEmbed().SetDescription(fmt.Sprintf("🏓 **%d**ms", Client.GetWSPing())).SetColor("00ADD8"),
+				},
+			})
+		} else if interaction.Data.Name == "links" {
+			interaction.Reply(classes.MessageData{
+				Embeds: []classes.Embed{
+					classes.NewEmbed().SetDescription("[Repo of godiscord](https://github.com/AYn0nyme/godiscord)\n[Repo of this bot](https://github.com/AYn0nyme/godiscord-support)").SetColor("00ADD8"),
+				},
+				Flags: []types.MessageFlag{
+					enums.MessageFlags.Ephemeral,
+				},
+			})
 		}
 	})
 
-	err := Client.Connect(os.Getenv("DISCORD_TOKEN"))
+	err := Client.Connect()
 	if err != nil {
 		return
 	}
